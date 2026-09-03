d3efc0281d1fed108c4fadd50abf13223432bd4c
Raghav Sathe <raghavgsathe9@gmail.com>
2026-09-03 20:05:04 +0530

    Add libopencm3 submodule

---
+++ app/inc/common-defines.h
@@ -0,0 +1,8 @@
+ #ifndef INC_COMMON_DEFINES_H
+ #define INC_COMMON_DEFINES_H
+
+ #include <stdint.h>
+ #include <stdbool.h>
+
+ #endif // INC_COMMMON_DEFINES_H
+ 
---
+++ src/firmware.c
@@ -0,0 +1,17 @@
+// This code will tell the processor to tell the gpio to turn led pin on and off in a loop
+
+ 
+
+int main (void) {
+
+    while (1) {                   // represents infinite loop to keep the program running , keep the cpu running as long it is on
+
+
+    }
+
+
+
+
+    // Never return
+    return 0;
+}
